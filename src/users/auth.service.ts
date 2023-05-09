525d666c4a31f817c73cfc23c570d7b7e8c057c6
rajutamanglama65 <raju@fusemachines.com>
2023-05-09 12:49:31 +0545

    feat: signup feature added

---
+++ src/users/auth.service.ts
@@ -0,0 +1,36 @@
+import { BadRequestException, Injectable } from "@nestjs/common";
+import { randomBytes, scrypt as _scrypt } from "crypto";
+import { promisify } from "util";
+import { UsersService } from "./users.service";
+
+const scrypt = promisify(_scrypt)
+
+@Injectable()
+export class AuthService {
+    constructor(private usersService: UsersService) {}
+
+    async signup(email: string, password: string) {
+        const existingUsers = await this.usersService.find(email)
+
+        if(existingUsers.length) {
+            throw new BadRequestException("User with this email already exists. ")
+        }
+
+        // generate a salt
+        const salt = randomBytes(8).toString("hex")
+
+        // hash the salt and password together
+        const hash = (await scrypt(password, salt, 32) as Buffer)
+
+        // join the hassed password and the salt together
+        const hassedPassword = salt + "." + hash.toString("hex")
+
+        // create a new user and save it
+        const user = await this.usersService.create(email, hassedPassword)
+
+        // return the user
+        return user;
+    }
+
+    async signin() {}
+}
